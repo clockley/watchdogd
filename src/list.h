d9971b50aa87a027cb5029ecc1a0dba3389e55c3
Christian Lcokley <clockley1@gmail.com>
2014-02-01 19:52:31 -0500

    '

--- src/list.h
+++ src/list.h
@@ -27,6 +27,11 @@
 
 #ifndef LIST_H_
 #define LIST_H_
+
+struct list {
+	struct list *next, *prev;
+};
+
 bool list_is_empty(void *arg_one);
 void list_move_tail(void *arg_one, void *arg_two);
 void list_move(void *arg_one, void *arg_two);
@@ -116,9 +121,7 @@ void list_add(void *arg_one, void *arg_two);
  * There are no requirements for a list head, any struct list can be a list
  * head.
  */
-struct list {
-	struct list *next, *prev;
-};
+
 
 /**
  * Alias of container_of
