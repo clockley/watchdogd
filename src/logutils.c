a26215b3ff18b52f8e4ab77e83b4b5088472e1c0
Christian Lockley <clockley1@gmail.com>
2014-03-11 18:50:07 -0400

    Update copyright info

--- src/logutils.c
+++ src/logutils.c
@@ -19,7 +19,7 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 /*
- * Copyright 2013 Christian Lockley
+ * Copyright 2013-2014 Christian Lockley
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may 
  * not use this file except in compliance with the License. You may obtain 
@@ -31,7 +31,7 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
  * implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * permissions and limitations under the License. 
  */
 #include "watchdogd.h"
 
