4e6347daa8d70244eaab98bc4ed94d2da1f7c0c2
Christian Lockley <clockley1@gmail.com>
2014-04-23 17:32:46 -0400

    add assert

--- src/init.c
+++ src/init.c
@@ -52,7 +52,7 @@ int LoadConfigurationFile(struct cfgoptions *s)
 	int tmp = 0;
 
 	config_init(&s->cfg);
-
+	assert(s->confile);
 	if (!config_read_file(&s->cfg, s->confile)
 	    && config_error_file(&s->cfg) == NULL) {
 		fprintf(stderr,
