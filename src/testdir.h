fb93ec8719d129d62f2756d568eba29fd9827759
Christian Lockley <clockley1@gmail.com>
2014-07-02 17:36:26 -0400

    Fix warning:
    ./testdir.h:1:9: warning: 'TESTDIR_H' is used as a header guard here, followed
          by #define of a different macro [-Wheader-guard]
            ^~~~~~~~~
    ./testdir.h:2:9: note: 'TESTDIR' is defined here; did you mean 'TESTDIR_H'?
            ^~~~~~~
            TESTDIR_H
    1 warning generated.

--- src/testdir.h
+++ src/testdir.h
@@ -1,5 +1,5 @@
 #ifndef TESTDIR_H
-#define TESTDIR
+#define TESTDIR_H
 int CreateLinkedListOfExes(const char *path, ProcessList * p);
 int ExecuteRepairScripts(ProcessList * p, struct cfgoptions *s);
 void FreeExeList(ProcessList * p);
