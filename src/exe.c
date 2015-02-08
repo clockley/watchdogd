94e021462f164179907e274bf246b88e81a20204
Christian Lockley <clockley1@gmail.com>
2015-02-08 00:20:14 -0500

    Revert "use raw syscalls"
    This reverts commit 5ca33c78f91740831f5cfdc3baa29afc97e038f5.

--- src/exe.c
+++ src/exe.c
@@ -57,7 +57,7 @@ int SpawnAttr(spawnattr_t *spawnattr, const char *file, const char *args, ...)
 		return -1;
 	}
 
-	pid_t pid = signal_safe_fork(); //default fork is not signal safe on linux
+	pid_t pid = fork();
 
 	switch (pid) {
 	case -1:
@@ -70,22 +70,17 @@ int SpawnAttr(spawnattr_t *spawnattr, const char *file, const char *args, ...)
 
 			OnParentDeathSend(SIGKILL);
 
-			pid_t worker = signal_safe_fork();
+			pid_t worker = fork();
 
 			if (worker == 0) {
 				OnParentDeathSend(SIGKILL);
-
 				struct sched_param param;
 				param.sched_priority = 0;
-				if (signal_safe_sched_setscheduler(getpid_nocache(), SCHED_OTHER, &param) < 0) {
-					Logmsg(LOG_ERR, "signal_safe_sched_setscheduler failed: %s", MyStrerror(errno));
-				}
 
-				int ret = 0;
+				sched_setscheduler(getpid(), SCHED_OTHER,
+						   &param);
 
-				signal_safe_nice(spawnattr->nice, &ret);
-
-				if (ret != 0) {
+				if (nice(spawnattr->nice) == -1) {
 					Logmsg(LOG_ERR, "nice failed: %s", MyStrerror(errno));
 				}
 
