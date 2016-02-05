0f7d25a64839c217e75a6723f75444b83a24b925
Christian Lockley <clockley1@gmail.com>
2016-02-05 11:38:54 -0500

    Add script that automaticly installs dependencies on Ubuntu and Fedora

---
+++ install_dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+distro=$(lsb_release -i|cut -f 2 -d ":"|tr -d "\t")
+
+SUDO=''
+if (( $EUID != 0))
+then
+	SUDO='sudo'
+fi
+
+if [ $distro == "Fedora" ]
+then
+	$SUDO dnf -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel
+	exit
+fi
+
+if [ $distro == "Ubuntu" ]
+then
+	$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make
+	exit
+fi
