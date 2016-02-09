409001bb4497f6d6631c34c201bb060f776d32d0
Christian Lockley <clockley1@gmail.com>
2016-02-09 00:38:14 -0500

    i. Support older fedora systems ii. add error message

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,7 +10,7 @@ fi
 
 if [ $distro == "Fedora" ]
 then
-	$SUDO dnf -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel
+	$SUDO dnf -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel || $SUDO yum -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel
 	exit
 fi
 
@@ -19,3 +19,7 @@ then
 	$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make
 	exit
 fi
+
+echo "Your version of linux is not supported"
+echo "If your are able to install this program on the version of linux you are"
+echo "running please email the commands used. clockley1@gmail.com"
