e07167810cc62c29814949b3c0697847efb79672
Christian Lockley <clockley1@gmail.com>
2016-06-09 15:43:23 -0400

    Add spinner to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
-autoreconf -fiv
-rm -rf autom4te.cache
+#!/bin/bash
+spinner()
+{
+    local pid=$!
+    local delay=0.5
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+(autoreconf -fi; rm -rf autom4te.cache) & spinner
