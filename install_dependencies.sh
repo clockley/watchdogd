003f7ce90de5c31f815baf7ea911bcf3e367f1be
Christian Lockley <clockley1@gmail.com>
2016-06-21 20:11:43 -0400

    Add missing dependency

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -18,7 +18,7 @@ if [ $ID == "fedora" ]
 then
 	if [[ "wheel" != $(groups|grep -o wheel) ]]
 	then
-		su -c 'dnf -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel'
+		su -c 'dnf -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel dbus-devel'
 		exit
 	fi
 	$SUDO dnf -y install libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel
@@ -27,19 +27,19 @@ fi
 
 if [ $ID == "ubuntu" ]
 then
-	$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make
+	$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make libdbus-1-dev
 	exit
 fi
 
 if [ $ID == "debian" ]
 then
-	$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make
+	$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make  libdbus-1-dev
 	exit
 fi
 
 if [ $ID == "opensuse" ]
 then
-	su -c ' zypper install -yl libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel'
+	su -c ' zypper install -yl libconfig-devel zlib-devel automake autoconf libmount-devel gcc make liboping-devel systemd-devel   dbus-1-devel'
 	exit
 fi
 
@@ -49,13 +49,13 @@ then
 	do
 		if [ $i == "debian" ]
 		then
-			$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make
+			$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make  libdbus-1-dev
 			exit
 		fi
 
 		if [ $i == "ubuntu" ]
 		then
-			$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make
+			$SUDO apt-get -y install libconfig-dev liboping-dev zlib1g-dev automake autoconf libsystemd-dev libmount-dev gcc make libdbus-1-dev
 			exit
 		fi
 	done
