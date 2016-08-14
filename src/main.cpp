20ce11f79eefdfda2e0d3039be75666b2876b16b
Christian Lockley <clockley1@gmail.com>
2016-08-14 12:02:23 -0400

    Revert "Fix unblock code"
    This reverts commit 7cc3450eb5289624018f77a41a878ed87c61725c.

--- src/main.cpp
+++ src/main.cpp
@@ -379,6 +379,8 @@ init:
 		sd_bus_flush_close_unref(bus);
 	}
 
+	pthread_sigmask(SIG_UNBLOCK, &set, NULL);
+
 	int fildes[2] = {0};
 	pipe(fildes);
 	pid = fork();
@@ -418,7 +420,6 @@ init:
 	sd_notifyf(0, "READY=1\n" "MAINPID=%lu", (unsigned long)getpid());
 
 	do {
-		pthread_sigmask(SIG_UNBLOCK, &set, NULL);
 		pause();
 		switch (sigValue) {
 		case SIGHUP:
