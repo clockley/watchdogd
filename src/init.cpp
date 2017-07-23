67bb4cf03b208d6a6762a8a655fea9ac1aba286f
Christian Lockley <clockley1@gmail.com>
2017-07-23 01:37:36 -0400

    add support for codenames and use v4 codename

--- src/init.cpp
+++ src/init.cpp
@@ -236,7 +236,7 @@ int CheckPriority(int priority)
 
 int PrintVersionString(void)
 {
-	printf("%s\n", PACKAGE_STRING);
+	printf("%s %s\n", PACKAGE_STRING, CODENAME);
 	printf("Copyright 2013-2017 Christian Lockley. All rights reserved.\n");
 	printf("Licensed under the Apache License, Version 2.0.\n");
 	printf("There is NO WARRANTY, to the extent permitted by law.\n");
